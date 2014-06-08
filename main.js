a1f21d27d521551430a8259d55b05d482a85539f
hung-phan <colorvisavn@gmail.com>
2014-06-08 22:10:04 +0700

    update for browserify

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * Module Dependencies
+ * Reference
+ * By https://github.com/ericclemmons/grunt-react/blob/master/main.js
+ */
+
+var Transformers  = require('./lib/transformers.js');
+
+/**
+ * Exports
+ */
+
+module.exports = Transformers;
